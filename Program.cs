2d816f44ad589728e9f91d4ef57c6501ee1eed94
tudorg <tudorg@indsoft.ro>
2023-10-05 17:50:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Curs3Prob2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceți un cuvânt: ");
+            string cuvant = Console.ReadLine();
+
+            bool estePalindrom = VerificaPalindrom(cuvant);
+
+            if (estePalindrom)
+            {
+                Console.WriteLine($"{cuvant} este palindrom.");
+            }
+            else
+            {
+                Console.WriteLine($"{cuvant} nu este palindrom.");
+            }
+        }
+
+        static bool VerificaPalindrom(string cuvant)
+        {
+            cuvant = cuvant.ToLower();
+
+            int lungime = cuvant.Length;
+            for (int i = 0; i < lungime / 2; i++)
+            {
+                if (cuvant[i] != cuvant[lungime - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
